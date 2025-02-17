8329942717e6553709a606d939855a828b52b9a4
Nikita Gusakov <n.g3nnadevich@gmail.com>
2025-02-17 21:23:50 +0300

    Fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const greeting = "Welcome to the Brain Games!";
-console.log(greeting);
-
 import greetUser from '../src/cli.js';
-greetUser ();
+
+const greeting = 'Welcome to the Brain Games!';
+console.log(greeting);
+greetUser();
