e85857e14bc2e5cbc338bf6aa123ad8652ffcfc9
Nikita Gusakov <n.g3nnadevich@gmail.com>
2025-02-26 21:41:43 +0300

    Fix bugs

--- src/Games/even.js
+++ src/Games/even.js
@@ -1,9 +1,7 @@
-import { getRandomNumber } from '../math.js';
+import { getRandomNumber, isEven } from '../math.js';
 import game from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-const isEven = (number) => number % 2 === 0;
 
 const questionAndAnswer = () => {
   const question = String(getRandomNumber(1, 100));
